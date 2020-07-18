92de4cf647f3cf44d31665507c25faac1736f0fd
Grzegorz Sikora <greesik@gmail.com>
2020-07-18 19:36:33 +0200

    added units

--- app/js/app.js
+++ app/js/app.js
@@ -5,23 +5,31 @@ let tBody = document.querySelector("tbody");
 let categoryInput = document.querySelector("#categories");
 let itemInput = document.querySelector("#item");
 let quantityInput = document.querySelector("#quantity");
+let unitInput = document.querySelector("#pieces");
 
 
 addButton.addEventListener("click", function(){
-    let tr1 = document.createElement("tr");
-    let td1 = document.createElement("td");
-    tr1.appendChild(td1);
-    td1.innerText = itemInput.value;
-    let td2 = document.createElement("td");
-    tr1.appendChild(td2);
-    td2.innerText = quantityInput.value;
-    let td3 = document.createElement("td");
-    td3.innerText = categoryInput.options[categoryInput.selectedIndex].text;
-    tr1.appendChild(td3);
-    let td4 = document.createElement("td");
-    td4.innerHTML = "<a href='#' class='delete'>Usuń</a>";
-    tr1.appendChild(td4);
-    tBody.appendChild(tr1);
+    let tableRow = document.createElement("tr");
+    let item = document.createElement("td");
+    tableRow.appendChild(item);
+    item.innerText = itemInput.value;
+    let quantity = document.createElement("td");
+    tableRow.appendChild(quantity);
+    quantity.innerText = quantityInput.value;
+    let unit = document.createElement("td");
+    tableRow.appendChild(unit);
+    if (unitInput.checked) {
+        unit.innerText = "szt."
+    } else {
+        unit.innerText = "kg"
+    }
+    let category = document.createElement("td");
+    category.innerText = categoryInput.options[categoryInput.selectedIndex].text;
+    tableRow.appendChild(category);
+    let deleteBtn = document.createElement("td");
+    deleteBtn.innerHTML = "<a href='#' class='delete'>Usuń</a>";
+    tableRow.appendChild(deleteBtn);
+    tBody.appendChild(tableRow);
 
     let deleteRow = document.querySelectorAll(".delete");
     for (let i=0; i<deleteRow.length; i++){
